66a6eeb775e581f507861dbb7d7e957071e2d21a
Alexandre Versmée <versmee.alexandre@gmail.com>
2019-12-04 18:47:01 +0100

    working on home's design

---
+++ index.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>FirstScreen</title>
+
+    <?php include './partials/metas.html' ?>
+    <?php include './partials/styles.html' ?>
+
+</head>
+<body>
+
+<?php include './partials/header.html' ?>
+
+
+
+
+
+	<!--<div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+		<ul class="orbit-container">
+			<button class="orbit-previous" aria-label="previous"><span class="show-for-sr">Previous Slide</span>&#9664;</button>
+			<button class="orbit-next" aria-label="next"><span class="show-for-sr">Next Slide</span>&#9654;</button>
+			<li class="orbit-slide is-active">
+				<img src="https://placehold.it/2000x750">
+			</li>
+			<li class="orbit-slide">
+				<img src="https://placehold.it/2000x750">
+			</li>
+			<li class="orbit-slide">
+				<img src="https://placehold.it/2000x750">
+			</li>
+			<li class="orbit-slide">
+				<img src="https://placehold.it/2000x750">
+			</li>
+		</ul>
+	</div>-->
+
+
+
+	<div class="rows">
+		<img src="images/home.PNG" alt="" title=""/>
+	</div>
+
+	<div class="row">
+		<div class="medium-6 columns medium-push-6">
+			<img class="thumbnail" src="https://placehold.it/750x350">
+		</div>
+		<div class="medium-6 columns medium-pull-6">
+			<h2>Our Agency, our selves.</h2>
+			<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="medium-4 columns">
+			<h3>Photoshop</h3>
+			<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+		</div>
+		<div class="medium-4 columns">
+			<h3>Javascript</h3>
+			<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+		</div>
+		<div class="medium-4 columns">
+			<h3>Marketing</h3>
+			<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+		</div>
+	</div>
+
+
+
+	<!-- FOOTER -->
+    <?php include './partials/footer.html' ?>
+
+
+<?php include './partials/scripts.html' ?>
+
+</body>
+</html>
+
+
+
+
